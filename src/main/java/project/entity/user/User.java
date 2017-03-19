42a69a750916e909158cb6366054933129c04e2f
ivan <ivanmikhailenka@gmail.com>
2017-03-19 21:04:25 +0300

    add hibernate to pom.xml, configure tomcat, configure hibernate

---
+++ src/main/java/project/entity/user/User.java
@@ -0,0 +1,52 @@
+package project.entity.user;
+
+
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+
+/**
+ * Created by Ivan on 19.03.2017.
+ */
+@DynamicUpdate
+@Table(name = "user")
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", unique = true, nullable = false)
+  private int id;
+
+  @Column(name = "login")
+  private String login;
+
+  @Column(name = "mail")
+  private String mail;
+
+  public User() {
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getLogin() {
+    return login;
+  }
+
+  public void setLogin(String login) {
+    this.login = login;
+  }
+
+  public String getMail() {
+    return mail;
+  }
+
+  public void setMail(String mail) {
+    this.mail = mail;
+  }
+}
